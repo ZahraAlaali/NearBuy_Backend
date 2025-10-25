04c5a63469c230a2884ab60c0f0f82d68db8db4a
ZahraAlaali <zooz.alaali03@icloud.com>
2025-10-25 20:33:09 +0300

    Store model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose")
 
 const userSchema = require("./User")
+const storeSchema = require("./Store")
 
 const User = mongoose.model("User", userSchema)
+const Store = mongoose.model("Store", storeSchema)
 
 module.exports = {
   User,
+  Store,
 }
