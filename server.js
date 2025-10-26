e975048f69990cd369efc16969f320a17f7110c0
ZahraAlaali <zooz.alaali03@icloud.com>
2025-10-26 16:37:48 +0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const logger = require("morgan")
-// const cors = require("cors")
+const cors = require("cors")
 
 const AuthRouter = require("./routes/AuthRouter")
 const ItemRouter = require("./routes/ItemRouter")
@@ -12,7 +12,7 @@ const db = require("./db")
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
